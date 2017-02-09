ccde5eb0e9c54546b2ad39193d99c7e46ec7cf34
rahmatridhamus <rahmatridham@hotmail.com>
2017-02-09 20:12:47 +0700

    mantap

--- app/src/main/java/com/example/rahmatridham/makassar90/FragmentSchedule.java
+++ app/src/main/java/com/example/rahmatridham/makassar90/FragmentSchedule.java
@@ -6,6 +6,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 
 
 /**
@@ -13,6 +18,9 @@ import android.view.ViewGroup;
  */
 public class FragmentSchedule extends Fragment {
 
+    public static TabLayout tabLayout;
+    public static ViewPager viewPager;
+    public static int int_items = 2 ;
 
     public FragmentSchedule() {
         // Required empty public constructor
@@ -23,7 +31,71 @@ public class FragmentSchedule extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment_schedule, container, false);
+        View x = inflater.inflate(R.layout.fragment_fragment_schedule, container, false);
+        tabLayout = (TabLayout) x.findViewById(R.id.tabs);
+        viewPager = (ViewPager) x.findViewById(R.id.viewpager);
+
+        viewPager.setAdapter(new MyAdapter(getChildFragmentManager()));
+
+        /**
+         * Now , this is a workaround ,
+         * The setupWithViewPager dose't works without the runnable .
+         * Maybe a Support Library Bug .
+         */
+
+        tabLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                tabLayout.setupWithViewPager(viewPager);
+            }
+        });
+
+        return x;
+    }
+
+    class MyAdapter extends FragmentPagerAdapter {
+
+        public MyAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        /**
+         * Return fragment with respect to Position .
+         */
+
+        @Override
+        public Fragment getItem(int position)
+        {
+            switch (position){
+                case 0 : return new Sabtu();
+                case 1 : return new Minggu();
+
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+
+            return int_items;
+
+        }
+
+        /**
+         * This method returns the title of the tab according to the position.
+         */
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+
+            switch (position){
+                case 0 :
+                    return "Sabtu"+"\n 22 Feb 2017";
+                case 1 :
+                    return "Minggu"+"\n 23 Feb 2017";
+            }
+            return null;
+        }
     }
 
 }
